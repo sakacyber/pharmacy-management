7ed0e050e02c946275c9f4c723b85cfafb57c82a
sakacyber <chansakleng007@gmail.com>
2023-03-31 15:48:28 +0700

    update ui

--- app/Http/Controllers/Admin/PatientCrudController.php
+++ app/Http/Controllers/Admin/PatientCrudController.php
@@ -21,7 +21,7 @@ class PatientCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,24 +33,32 @@ class PatientCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
-        
+
 
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
+
+        CRUD::column('#')->type('row_number');
+        CRUD::column('name');
+        CRUD::column('gender');
+        CRUD::column('age');
+        CRUD::column('address');
+        CRUD::column('enter_date');
+        CRUD::column('description');
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -58,18 +66,18 @@ class PatientCrudController extends CrudController
     {
         CRUD::setValidation(PatientRequest::class);
 
-        
+
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
